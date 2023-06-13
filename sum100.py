2a6fe81beec69da35b6c048e330512ef59c23ff8
AlexNol03 <136399183+AlexNol03@users.noreply.github.com>
2023-06-12 18:22:27 -0700

    Add files via upload

---
+++ sum100.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 17:39:48 2023
+
+@author: be
+"""
+
+sum100=0
+
+for i in range(100):
+    sum100 = sum100 + (i+1)
+    
+print(f'sum of first 100 integers is {sum100}')
